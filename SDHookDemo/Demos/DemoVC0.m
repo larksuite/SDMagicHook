d87f49d655fdb96e26b1d964d7b61d2ef31b69d8
gaoshaodong <gaoshaodong@bytedance.com>
2020-06-28 22:58:33 +0800

    调整demo

--- SDHookDemo/Demos/DemoVC0.m
+++ SDHookDemo/Demos/DemoVC0.m
@@ -7,60 +7,14 @@
 //
 
 #import "DemoVC0.h"
-#import <SDMagicHook.h>
+#import "SDMagicHook.h"
 #import "SDAutoLayout.h"
-#import <objc/runtime.h>
-
-
-@interface A : NSObject
-- (void)foo;
-@end
-
-@implementation A
-- (void)foo {
-    NSLog(@"%s", __PRETTY_FUNCTION__);
-}
-@end
-
-@interface B : A @end
-
-@implementation B
-- (void)foo {
-    NSLog(@"%s", __PRETTY_FUNCTION__);
-    [super foo];
-}
-@end
-
-
-
 
 @interface DemoVC0 ()
-
-@property (nonatomic, assign) int num;
-@property (nonatomic, assign) int age;
 
 @end
 
 @implementation DemoVC0
-{
-    NSTimer *_t1;
-    NSTimer *_t2;
-}
-
-- (void)setNum:(int)num {
-    _num = num;
-}
-
-- (void)setAge:(int)age {
-    _age = age;
-}
-
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ([super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-
-    }
-    return self;
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -74,116 +28,6 @@
     [btn addTarget:self action:@selector(findOutTheOneWhoChangesMyBackgroudcolor:) forControlEvents:UIControlEventTouchUpInside];
     btn.backgroundColor = UIColor.redColor;
     btn.frame = CGRectMake(100, 100, 200, 30);
-
-
-    [self addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-
-//    [self hookMethod:@selector(setAge:) impBlock:^(typeof(self) vc, int age){
-//        [vc callOriginalMethodInBlock:^{
-//            vc.age = age;
-//        }];
-//        NSLog(@">> %d", age);
-//    }];
-//
-//    [self hookMethod:@selector(setAge:) impBlock:^(typeof(self) vc, int age){
-//        [vc callOriginalMethodInBlock:^{
-//            vc.age = age;
-//        }];
-//        NSLog(@">> %d", age);
-//    }];
-
-    [self addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-
-//    [self hookMethod:@selector(setNum:) impBlock:^(typeof(self) vc, int num) {
-//        [vc callOriginalMethodInBlock:^{
-//            vc.num = num;
-//        }];
-//        NSLog(@">> %d", num);
-//    }];
-//
-//    [self hookMethod:@selector(setNum:) impBlock:^(typeof(self) vc, int num) {
-//        [vc callOriginalMethodInBlock:^{
-//            vc.num = num;
-//        }];
-//        NSLog(@">> %d", num);
-//    }];
-//
-//    [self hookMethod:@selector(setNum:) impBlock:^(typeof(self) vc, int num) {
-//        [vc callOriginalMethodInBlock:^{
-//            vc.num = num;
-//        }];
-//        NSLog(@">> %d", num);
-//    }];
-//
-//    [self hookMethod:@selector(setNum:) impBlock:^(typeof(self) vc, int num) {
-//        [vc callOriginalMethodInBlock:^{
-//            vc.num = num;
-//        }];
-//        NSLog(@">> %d", num);
-//    }];
-    
-    [self addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew context:nil];
-    [self addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-
-//    [self aspect_hookSelector:@selector(setAge:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> info, int aa){
-//        printf(">>>> %d", aa);
-//    } error:nil];
-
-//    [self addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew context:nil];
-//    [self addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-//
-//
-//    A *a = [A new];
-//    [a hookMethod:@selector(foo) impBlock:^(typeof(a) this){
-//        [this callOriginalMethodInBlock:^{
-//            [this foo];
-//        }];
-//    }];
-//    B *b = [B new];
-//    [b hookMethod:@selector(foo) impBlock:^(typeof(b) this){
-//        [this callOriginalMethodInBlock:^{
-//            [this foo];
-//        }];
-//    }];
-//
-//    [a foo];
-//    [b foo];
-
-    _t1 = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(test1) userInfo:nil repeats:YES];
-
-//    _t2 = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(test2) userInfo:nil repeats:YES];
-
-    [[NSRunLoop mainRunLoop] addTimer:_t1 forMode:NSRunLoopCommonModes];
-//    [[NSRunLoop mainRunLoop] addTimer:_t2 forMode:NSRunLoopCommonModes];
-}
-
-- (void)test1 {
-    static int a = 1;
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        self.num = 100000 + a;
-        self.age = 200000 + a;
-        ++a;
-        NSLog(@"------------------------------");
-    });
-}
-
-- (void)test2 {
-    static int a = 1;
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        [self addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew context:nil];
-        [self addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-        NSLog(@"AAAAAAAAAAAAAAAAAAAAAAAAAAAAA %d", a++);
-    });
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
-    NSLog(@">> %@ >> %@", keyPath, change);
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    self.num = 11;
-    self.age = 12;
-    [self test2];
 }
 
 - (void)findOutTheOneWhoChangesMyBackgroudcolor:(UIButton *)btn {
--- SDHookDemo/Others/AppDelegate.m
+++ SDHookDemo/Others/AppDelegate.m
@@ -7,66 +7,15 @@
 //
 
 #import "AppDelegate.h"
-#import <SDMagicHook.h>
-
-@interface Test : NSObject
-@property (nonatomic, assign) int num;
-@property (nonatomic, assign) int height;
-@end
-
-@implementation Test
-
-- (void)setNum:(int)num {
-    _num = num;
-}
-
-@end
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-{
-    Test *_test;
-}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    _test = [Test new];
-    [_test addObserver:self forKeyPath:@"num" options:NSKeyValueObservingOptionNew context:nil];
-//    _test.num = 10;
-//
-//    _test.num = 1122;
-
-
-//    [_test willChangeValueForKey:@"num"];
-//    [_test didChangeValueForKey:@"num"];
-
-
-//    [_test hookMethod:@selector(setHeight:) impBlock:^(typeof(self->_test) this, int height) {
-//        [this callOriginalMethodInBlock:^{
-//            [this setHeight:height];
-//        }];
-//    }];
-
-    [_test addObserver:self forKeyPath:@"height" options:NSKeyValueObservingOptionNew context:nil];
-
-    _test.height = 100;
-
     return YES;
 }
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
-    NSLog(@">> %@, %@", keyPath, change);
-}
-
-
-#pragma mark - UISceneSession lifecycle
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    _test.num++;
-}
-
 
 @end
